654ec8921b46f596c2dc357b67f83b44848231ad
Dawitlior <dawitlior777@gmail.com>
2022-06-18 17:21:27 +0300

    add

--- main.js
+++ main.js
@@ -1,50 +1,64 @@
-function equalFunction(){
-let array = []
-let operators = []
-  for(let i = 0 ; i<result.value.length;i++){
-    let numbers+=result.value[i]
-switch (numbers) {
-  case "+":
-    array.push(numbers);
-    numbers = "";
-    operators.push(numbers[i]);
-    break;
-  case "-":
-        array.push(numbers);
-        numbers = "";
-        operators.push(numbers[i]);
-  break;
-  case "*":
-        array.push(numbers);
-    numbers = "";
-    operators.push(numbers[i]);
-    break;
-    case "/":
-          array.push(numbers);
-    numbers = "";
-    operators.push(numbers[i]);
-  default:
-    break;
-}
-  }
-}
-equalFunction();
-
-function showNumbers(num){
-  result.value += num.value
-}
+// function equalFunction() {
+//   let array = [];
+//   let operators = [];
+//   let result = document.getElementById("result");
+//   for (let i = 0; i < result.value.length; i++) {
+//     let numbers = +result.value[i];
+//     switch (numbers) {
+//       case "+":
+//         array.push(numbers);
+//         numbers = "";
+//         operators.push(numbers[i]);
+//         break;
+//       case "-":
+//         array.push(numbers);
+//         numbers = "";
+//         operators.push(numbers[i]);
+//         break;
+//       case "*":
+//         array.push(numbers);
+//         numbers = "";
+//         operators.push(numbers[i]);
+//         break;
+//       case "/":
+//         array.push(numbers);
+//         numbers = "";
+//         operators.push(numbers[i]);
+//       default:
+//         break;
+//     }
+//   }
+// }
 
 
-// function calculate() {
+// let result = document.getElementById("result");
+// let showNumbers = (number) => {
+//   result.value = result.value + number;
+//   result.style.fontSize = 40 + "px";
+// };
+
+// let equalFunction = ()=>{
+//   try{
+//     result.value=eval(result.value)
+//   }
+//   catch(err){
+//     alert('Enter a valid result');
+//   }
+// }
+// function del(){
+//   result.value = " ";
+// }
+// function equalFunction() {
+
 //   switch () {
 //     case "+":
 //       result.innerHTML = 
 //       break;
 //     case "-":
-//       result.innerHTML = 
+//       result.innerHTML =
 //       break;
 //     case "/":
-//       result.innerHTML = 
+//       result.innerHTML =
 //       break;
 //     case "*":
 //       result.innerHTML = firstNumber.value * secondNumber.value;
@@ -54,22 +68,13 @@ function showNumbers(num){
 //   }
 // }
 
-
-
-
-
-
-
-
-
-
 // let btn = document.getElementsByClassName("btn");
 // for (const iterator of btn) {
 //   switch (iterator) {
 //     case 2 * 2:
 //       div.innerHTML = iterator
 //       break;
-  
+
 //     default:
 //       break;
 //   }
