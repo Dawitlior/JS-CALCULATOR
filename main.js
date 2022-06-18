51f0740eb77dd1b76c32109274fde069ac882f21
Dawitlior <dawitlior777@gmail.com>
2022-06-18 17:24:58 +0300

    finish

--- main.js
+++ main.js
@@ -1,81 +1,18 @@
-// function equalFunction() {
-//   let array = [];
-//   let operators = [];
-//   let result = document.getElementById("result");
-//   for (let i = 0; i < result.value.length; i++) {
-//     let numbers = +result.value[i];
-//     switch (numbers) {
-//       case "+":
-//         array.push(numbers);
-//         numbers = "";
-//         operators.push(numbers[i]);
-//         break;
-//       case "-":
-//         array.push(numbers);
-//         numbers = "";
-//         operators.push(numbers[i]);
-//         break;
-//       case "*":
-//         array.push(numbers);
-//         numbers = "";
-//         operators.push(numbers[i]);
-//         break;
-//       case "/":
-//         array.push(numbers);
-//         numbers = "";
-//         operators.push(numbers[i]);
-//       default:
-//         break;
-//     }
-//   }
-// }
 
+let result = document.getElementById("result");
+let showNumbers = (number) => {
+  result.value = result.value + number;
+  result.style.fontSize = 40 + "px";
+};
 
-// let result = document.getElementById("result");
-// let showNumbers = (number) => {
-//   result.value = result.value + number;
-//   result.style.fontSize = 40 + "px";
-// };
-
-// let equalFunction = ()=>{
-//   try{
-//     result.value=eval(result.value)
-//   }
-//   catch(err){
-//     alert('Enter a valid result');
-//   }
-// }
-// function del(){
-//   result.value = " ";
-// }
-// function equalFunction() {
-
-//   switch () {
-//     case "+":
-//       result.innerHTML = 
-//       break;
-//     case "-":
-//       result.innerHTML =
-//       break;
-//     case "/":
-//       result.innerHTML =
-//       break;
-//     case "*":
-//       result.innerHTML = firstNumber.value * secondNumber.value;
-//       break;
-//     default:
-//       break;
-//   }
-// }
-
-// let btn = document.getElementsByClassName("btn");
-// for (const iterator of btn) {
-//   switch (iterator) {
-//     case 2 * 2:
-//       div.innerHTML = iterator
-//       break;
-
-//     default:
-//       break;
-//   }
-// }
+let equalFunction = ()=>{
+  try{
+    result.value=eval(result.value)
+  }
+  catch(err){
+    alert('Enter a valid result');
+  }
+}
+function del(){
+  result.value = " "
+}
