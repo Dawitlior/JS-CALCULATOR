15d39e051a66f7c40be45acbf0d752ea287906b5
Dawitlior <dawitlior777@gmail.com>
2022-06-18 19:52:13 +0300

    add

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-
 let result = document.getElementById("result");
 let showNumbers = (number) => {
   result.value += number;
   result.style.fontSize = 40 + "px";
 };
-
 let equalFunction = ()=>{
   try{
     result.value=eval(result.value)
